394f7d97cf3b88730c9ebf016eed72ea2ff2bff7
Cesare12 <591576872@qq.com>
2020-10-14 11:27:29 +0800

    Create 283.js

---
+++ code/283.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let index = 0;
+    for(let i in nums){
+        if(nums[i]){
+            if(i !== index){
+                [nums[i], nums[index]] = [nums[index], nums[i]];
+                index++;
+            } else {
+                index++;
+            }
+        }
+    }
+};
