c5dc69f05f8bca4ab15958fe81e67fcbde7bb374
Cesare12 <591576872@qq.com>
2020-10-14 13:49:53 +0800

    Update 283.js

--- code/283.js
+++ code/283.js
@@ -2,6 +2,20 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+var moveZeroes = function(nums) {
+    let index = 0;
+    for(let i in nums){
+        if(nums[i]){
+            [nums[i], nums[index]] = [nums[index], nums[i]];
+            index++;
+        }
+    }
+};
+
+
+//optimize
 var moveZeroes = function(nums) {
     let index = 0;
     for(let i in nums){
@@ -16,13 +30,3 @@ var moveZeroes = function(nums) {
     }
 };
 
-
-var moveZeroes = function(nums) {
-    let index = 0;
-    for(let i in nums){
-        if(nums[i]){
-            [nums[i], nums[index]] = [nums[index], nums[i]];
-            index++;
-        }
-    }
-};
