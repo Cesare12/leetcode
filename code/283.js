f4a4a3889c02be13c5ea9de8ff1dd7d593e1d744
Cesare12 <591576872@qq.com>
2020-10-14 13:35:18 +0800

    Update 283.js

--- code/283.js
+++ code/283.js
@@ -21,8 +21,8 @@ var moveZeroes = function(nums) {
     let index = 0;
     for(let i in nums){
         if(nums[i]){
-                [nums[i], nums[index]] = [nums[index], nums[i]];
-                index++;
+            [nums[i], nums[index]] = [nums[index], nums[i]];
+            ndex++;
         }
     }
 };
