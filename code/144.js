b50ef09cfbb21e690c5281652e70e6c2833a7429
Cesare12 <591576872@qq.com>
2021-02-17 15:20:06 +0800

    Create 144.js

---
+++ code/144.js
@@ -0,0 +1,55 @@
+给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+
+示例 1：
+输入：root = [1,null,2,3]
+输出：[1,2,3]
+
+示例 2：
+输入：root = []
+输出：[]
+
+示例 3：
+输入：root = [1]
+输出：[1]
+
+示例 4：
+输入：root = [1,2]
+输出：[1,2]
+
+示例 5：
+输入：root = [1,null,2]
+输出：[1,2]
+ 
+提示：
+
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ 
+var preorderTraversal = function (root) {
+    let res = [];
+    var traversal = function (node) {
+        if (node) {
+            res.push(node.val);
+            traversal(node.left);
+            traversal(node.right);
+        }
+    }
+    traversal(root)
+    return res
+};
