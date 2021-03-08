c1adc7ab4d48c1418052d91a91d4f32739604712
Cesare12 <591576872@qq.com>
2021-03-08 19:25:15 +0800

    Update 239.js

--- code/239.js
+++ code/239.js
@@ -50,21 +50,23 @@ var maxSlidingWindow = function (nums, k) {
 };
 
 var maxSlidingWindow = function (nums, k) {
- if(k===1) return nums;
- const q = [];
- for(let i=0;i<k;i++){
-   while(nums[q.slice(-1)] < nums[i] && q.length !== 0){q.pop()}
-   q.push(i);
- }
- 
- const res = [nums[q[0]]];
- const len = nums.length - k;
- for(let i=1;i<=len;i++){
-  if(q[0] <i) q.shift(q[0])
-   while(nums[q.slice(-1)] < nums[i+k-1] && q.length !== 0){q.pop()}
-   if(q.length<=k) q.push(i+k-1)
-  
-   res.push(nums[q[0]]);
- }
- return res
+    if (k === 1) return nums;
+    const q = [];
+    for (let i = 0; i < k; i++) {
+        while (nums[q.slice(-1)] < nums[i] && q.length !== 0) { q.pop() }
+        q.push(i);
+    }
+    const res = [nums[q[0]]];
+    const len = nums.length - k;
+    for (let i = 1; i <= len; i++) {
+        if (q[0] < i)
+            q.shift(q[0])
+        while (nums[q.slice(-1)] < nums[i + k - 1] && q.length !== 0) {
+            q.pop()
+        }
+        if (q.length <= k)
+            q.push(i + k - 1)
+        res.push(nums[q[0]]);
+    }
+    return res
 }
