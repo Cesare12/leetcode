9b84ffb4539f417e9114fca1e3324c2f01621790
Cesare12 <591576872@qq.com>
2022-09-04 18:24:29 +0800

    Update 283.c

--- code/283.c
+++ code/283.c
@@ -1,5 +1,5 @@
 
-/* 	一次遍历，使用index记录 值为零的元素位置，遇到 非零元素，将当前元素和index位置元素交换，index向后移动*/
+/* 一次遍历，使用index记录 值为零的元素位置，遇到 非零元素，将当前元素和index位置元素交换，index向后移动 */
 void moveZeroes(int* nums, int numsSize) {
   int index = 0;
   for (int i = 0; i < numsSize; i++) {
